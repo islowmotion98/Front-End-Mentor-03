9c0509519e6b3b4b8a3586d260393118c1da4980
islowmotion98 <109019417+islowmotion98@users.noreply.github.com>
2022-09-01 06:40:01 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+const slide = document.getElementById('isso')
+
+const button2 = document.getElementById('change-color')
+const buttons = document.getElementById('change')
+const button = document.getElementById('change2')
+
+const card2 = document.getElementById('card2')
+
+const div3h2 = document.getElementById('title32')
+const div3h1 = document.getElementById('title31')
+const div2h2 = document.getElementById('title22')
+const div2h1 = document.getElementById('title21')
+const div1h2 = document.getElementById('title12')
+const div1h1 = document.getElementById('title11')
+const troca = document.querySelector('.slider');
+    troca.onclick = function(){
+      div1h1.classList.toggle('disable')
+      div1h2.classList.toggle('disable')
+      div2h1.classList.toggle('disable')
+      div2h2.classList.toggle('disable')
+      div3h1.classList.toggle('disable')
+      div3h2.classList.toggle('disable')
+      card2.classList.toggle('box2-toggle')
+      buttons.classList.toggle('box2-toggle')
+      button.classList.toggle('box2-toggle')
+      buttons.classList.toggle('a10')
+      button.classList.toggle('a10')
+      button2.classList.toggle('letra-cor')
+      slide.i
+      slide.classList.toggle('box2-toggle')
+      
+    }
